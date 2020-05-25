64952db40c51a89ffa0cdd27571fff522eca12c6
Martin Achtermann <mdacht777@gmail.com>
2020-05-25 21:53:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
+  puts "R: red, "
+  puts "O: orange, "
+  puts "Y: yellow, "
+  puts "G: green, "
+  puts "B: blue, "
+  puts "I: indigo, "
+  puts "V: violet "
 end
